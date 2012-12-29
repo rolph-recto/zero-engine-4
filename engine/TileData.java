8194d6bdf53fea7c467f861aeb438dd2e738ee22
Rolph Recto <rolph.recto@gmail.com>
2012-12-29 18:47:57 -0500

    Added Tiletemplate class

--- engine/TileData.java
+++ engine/TileData.java
@@ -2,8 +2,28 @@ package engine;
 
 final class TileTemplate {
 	private String name;
-	private int id;
-	private int image_id; //index of the image used in Tileset
+	private int index; //index of the image used in Tileset
+	
+	public TileTemplate(String name, int index) {
+		this.name = name;
+		this.index = index;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getIndex() {
+		return this.index;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
 }
 
 public class TileData {
